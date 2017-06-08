e300f8adafbb9a1f119cbf64fa11807ea30f7886
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-08 19:52:33 +0900

    commit

---
+++ getwebrtcobjmember.js
@@ -0,0 +1,8 @@
+objMembers = {};
+['RTCCertificate', 'RTCDataChannel', 'RTCDataChannelEvent', 'RTCIceCandidate', 'RTCPeerConnection', 'RTCPeerConnectionIceEvent', 'RTCRtpContributingSource', 'RTCRtpReceiver', 'RTCSessionDescription', 'RTCStatsReport', 'webkitRTCPeerConnection'].forEach(className => {
+    objMembers[className] = Object.keys(window[className]).sort();
+});
+var json = JSON.stringify(objMembers, null, 2).replace(/"/g, '');
+var blob = new Blob([json], { type: 'text/plain' });
+dl.download = `WebRTC_Object_members.txt`;
+dl.href = URL.createObjectURL(blob);
