99fd7e50f214d0f683af93c4827953661ce5715a
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-08 20:15:30 +0900

    commit

--- docs/getwebrtcobjmember.js
+++ docs/getwebrtcobjmember.js
@@ -28,15 +28,18 @@ function getDocs() {
     return promise;
 }
 
-var data = WebIDLParse(docs);
-objMembers = {};
-Object.keys(data.Dictionary).forEach(className => {
-    objMembers[className] = Object.keys(window[className].prototype).sort();
+getDocs().then(docs=> {
+    var data = WebIDLParse(docs);
+    objMembers = {};
+    Object.keys(data.Dictionary).forEach(className => {
+        objMembers[className] = Object.keys(window[className].prototype).sort();
+    });
+    Object.keys(data.Interface).forEach(className => {
+        objMembers[className] = Object.keys(window[className].prototype).sort();
+    });
+    var json = JSON.stringify(objMembers, null, 2).replace(/"/g, '');
+    var blob = new Blob([json], { type: 'text/plain' });
+    dl.style.display = '';
+    dl.download = `WebRTC_Object_members.txt`;
+    dl.href = URL.createObjectURL(blob);
 });
-Object.keys(data.Interface).forEach(className => {
-    objMembers[className] = Object.keys(window[className].prototype).sort();
-});
-var json = JSON.stringify(objMembers, null, 2).replace(/"/g, '');
-var blob = new Blob([json], { type: 'text/plain' });
-dl.download = `WebRTC_Object_members.txt`;
-dl.href = URL.createObjectURL(blob);
