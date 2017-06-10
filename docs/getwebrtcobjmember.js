10b92a2a50cb4b69ffc1c4c44ff077462dc3d8e0
turbographics2000 <turbographics2000@yahoo.co.jp>
2017-06-11 04:23:38 +0900

    commit

--- docs/getwebrtcobjmember.js
+++ docs/getwebrtcobjmember.js
@@ -169,85 +169,74 @@ function buildTable() {
 
             implementData[browserName][version] = implementData[browserName][version];
             Object.keys(implementData[browserName][version]).sort().forEach(className => {
-                if (implementData[browserName][version][className] === null) {
-                    rows[className] = null;
-                } else {
-                    rows[className] = {};
-                    Object.keys(implementData[browserName][version][className]).sort().forEach(memberName => {
-                        if(memberName === 'addStream') debugger;
-                        rows[className][memberName] = implementData[browserName][version][className][memberName];
-                    });
-                }
+                rows[className] = {};
+                Object.keys(implementData[browserName][version][className]).sort().forEach(memberName => {
+                    if (memberName === 'addStream') debugger;
+                    rows[className][memberName] = implementData[browserName][version][className][memberName];
+                });
             });
         });
     });
 
     Object.keys(rows).sort().forEach(className => {
-        var memberNames = rows[className] === null ? null : Object.keys(rows[className]).sort();
+        var memberNames = Object.keys(rows[className]).sort();
+        if (memberNames.includes('addStream')) debugger;
         var classNameTR = document.createElement('tr');
         var classNameTD = document.createElement('td');
         classNameTD.textContent = className;
         classNameTD.classList.add('class-name');
-        if (memberNames === null) {
-            var notImplimentTD = document.createElement('td');
-            notImplimentTD.classList.add('class-notimpliment');
-            classNameTR.appendChild(classNameTD);
-            classNameTR.appendChild(notImplimentTD);
-            table.appendChild(classNameTR);
-        } else {
-            var arrow = document.createElement('div');
-            window.arrows[className] = arrow;
-            arrow.classList.add('arrow');
-            arrow.classList.add(className + 'arrow');
-            classNameTD.appendChild(arrow);
-            classNameTR.style.cursor = 'pointer';
-            classNameTR.appendChild(classNameTD);
-            Object.keys(implementData).sort().forEach(browserName => {
-                window.browserCounters[browserName] = window.browserCounters[browserName] || {};
-                Object.keys(implementData[browserName]).sort((a, b) => (+b) - (+a)).forEach(version => {
-                    var classImpCntTD = document.createElement('td');
-                    classImpCntTD.classList.add('imp-cnt');
-                    var specCnt = Object.keys(implementData[browserName][version][className] || {}).filter(x => implementData[browserName][version][className][x] === 'spec').length;
-                    if (memberNames.length) {
-                        classImpCntTD.style.background = heatColor(specCnt / memberNames.length)
-                        classImpCntTD.textContent = specCnt + ' / ' + memberNames.length;
-                        window.browserCounters[browserName][version] = window.browserCounters[browserName][version] || { specCnt: 0, memberCnt: 0 };
-                        window.browserCounters[browserName][version].specCnt += specCnt;
-                        window.browserCounters[browserName][version].memberCnt += memberNames.length;
-                    }
-                    classNameTR.appendChild(classImpCntTD);
-                });
+        var arrow = document.createElement('div');
+        window.arrows[className] = arrow;
+        arrow.classList.add('arrow');
+        arrow.classList.add(className + 'arrow');
+        classNameTD.appendChild(arrow);
+        classNameTR.style.cursor = 'pointer';
+        classNameTR.appendChild(classNameTD);
+        Object.keys(implementData).sort().forEach(browserName => {
+            window.browserCounters[browserName] = window.browserCounters[browserName] || {};
+            Object.keys(implementData[browserName]).sort((a, b) => (+b) - (+a)).forEach(version => {
+                var classImpCntTD = document.createElement('td');
+                classImpCntTD.classList.add('imp-cnt');
+                var specCnt = Object.keys(implementData[browserName][version][className] || {}).filter(x => implementData[browserName][version][className][x] === 'spec').length;
+                if (memberNames.length) {
+                    classImpCntTD.style.background = heatColor(specCnt / memberNames.length)
+                    classImpCntTD.textContent = specCnt + ' / ' + memberNames.length;
+                    window.browserCounters[browserName][version] = window.browserCounters[browserName][version] || { specCnt: 0, memberCnt: 0 };
+                    window.browserCounters[browserName][version].specCnt += specCnt;
+                    window.browserCounters[browserName][version].memberCnt += memberNames.length;
+                }
+                classNameTR.appendChild(classImpCntTD);
             });
-            classNameTR.onclick = function () {
-                var cn = this.firstChild.textContent;
-                Array.from(document.getElementsByClassName(this.firstChild.textContent + 'member')).forEach(elm => elm.classList.toggle('collapse'));
-                document.getElementsByClassName(this.firstChild.textContent + 'arrow')[0].classList.toggle('down');
-            }
-            table.appendChild(classNameTR);
-            memberNames.forEach(memberName => {
-                if (memberName === 'addStream') debugger;
-
-                var memberTR = document.createElement('tr');
-                memberTR.classList.add(className + 'member');
-                memberTR.classList.add('collapse');
-                var memberNameTD = document.createElement('td');
-                memberTR.classList.add('member-row');
-                memberTR.classList.add(rows[className][memberName]);
-                memberNameTD.classList.add('member-name');
-                memberNameTD.textContent = memberName;
-                memberTR.appendChild(memberNameTD);
-                Object.keys(implementData).sort().forEach(browserName => {
-                    Object.keys(implementData[browserName]).sort().reverse().forEach(version => {
-                        var memberTD = document.createElement('td');
-                        memberTD.id = browserName + version + className + memberName;
-                        memberTD.classList.add('member-null');
-                        window.tds[browserName + version + className + memberName] = memberTD;
-                        memberTR.appendChild(memberTD);
-                    });
-                });
-                table.appendChild(memberTR);
-            });
+        });
+        classNameTR.onclick = function () {
+            var cn = this.firstChild.textContent;
+            Array.from(document.getElementsByClassName(this.firstChild.textContent + 'member')).forEach(elm => elm.classList.toggle('collapse'));
+            document.getElementsByClassName(this.firstChild.textContent + 'arrow')[0].classList.toggle('down');
         }
+        table.appendChild(classNameTR);
+        memberNames.forEach(memberName => {
+            if (memberName === 'addStream') debugger;
+
+            var memberTR = document.createElement('tr');
+            memberTR.classList.add(className + 'member');
+            memberTR.classList.add('collapse');
+            var memberNameTD = document.createElement('td');
+            memberTR.classList.add('member-row');
+            memberTR.classList.add(rows[className][memberName]);
+            memberNameTD.classList.add('member-name');
+            memberNameTD.textContent = memberName;
+            memberTR.appendChild(memberNameTD);
+            Object.keys(implementData).sort().forEach(browserName => {
+                Object.keys(implementData[browserName]).sort().reverse().forEach(version => {
+                    var memberTD = document.createElement('td');
+                    memberTD.id = browserName + version + className + memberName;
+                    memberTD.classList.add('member-null');
+                    window.tds[browserName + version + className + memberName] = memberTD;
+                    memberTR.appendChild(memberTD);
+                });
+            });
+            table.appendChild(memberTR);
+        });
     });
 
     Object.keys(implementData).sort().forEach(browserName => {
